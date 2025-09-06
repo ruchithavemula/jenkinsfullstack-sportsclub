a6b17154e643da1cf81dae03f5797bfc7c8c7758
ruchithavemula <2300032021@kluniversity.in>
2025-09-06 17:22:15 +0530

    updated version

--- JenkinsFrontend/SportsClubFrontend/src/config.js
+++ JenkinsFrontend/SportsClubFrontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:2030/sportsclub-backend";
+// src/config.js
+export const BASE_URL = "/sportsclub-backend";
 export const API_URL = `${BASE_URL}/api/members`;
